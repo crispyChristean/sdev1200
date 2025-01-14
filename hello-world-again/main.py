8650e62551e7e519b5f188cc09187d0deacef8e7
Christian <christianespinozacel@westernwyoming.edu>
2025-01-14 03:01:38 +0000

    Ready to grade!!!

--- hello-world-again/main.py
+++ hello-world-again/main.py
@@ -1,8 +1,15 @@
 #
-# Name
-# Date
+# Christian Espinoza
+# 1/13/2024
 # Hello Word! Again
 # SDEV 1200 
 #
 
 # Use comments liberally throughout the program.
+
+
+#Create the variable here
+
+greeting = "Hello World!"
+
+print(f'{greeting}')
