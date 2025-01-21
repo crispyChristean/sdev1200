0db748ae4bef7d53610a87e638a57fd80c53695b
Christian <christianespinozacel@westernwyoming.edu>
2025-01-21 04:17:33 +0000

    Ready to Grade! :?

--- wi-fi-diagnostic-tree/main.py
+++ wi-fi-diagnostic-tree/main.py
@@ -1,8 +1,27 @@
 #
-# Name
-# Date
+# Christian Espinoza    
+# 01/20/2025
 # Wi-Fi Diagnostic Tree Programming Project
 # SDEV 1200 
 #
 
 # Use comments liberally throughout the program.
+
+#
+
+#Imports the system Module 
+import sys
+
+
+first = "Reboot the computer and try to connect.\nDid that fix the problem?"
+second = "Reboot the router and try to connect.\nDid that fix the problem?"
+third = "Make sure the cables between the router and modem are plugged in firmly.\nDid that fix the problem?"
+fourth = "Move the router to a new location.\nDid that fix the problem?"
+fifth = "Get a new router, or at least contact your service provider."
+
+for iteration in (first, second, third, fourth, fifth):
+    given = str(input(iteration))
+    if given == "Yes" or given == "yes":
+        break
+
+sys.exit()
