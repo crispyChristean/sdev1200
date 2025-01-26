73902a4be51f4e2186e0d137d82a7b1b78d4d73d
Christian <christianespinozacel@westernwyoming.edu>
2025-01-26 02:53:14 +0000

    htfkvgjfjychg

--- prime-numbers/instructions.md
+++ prime-numbers/instructions.md
@@ -3,3 +3,9 @@
 A prime number is a number that is only evenly divisible by itself and 1. For example, the number 5 is prime because it can only be evenly divided by 1 and 5. The number 6, however, is not prime because it can be divided evenly by 1, 2, 3, and 6. 
 
 Write a Boolean function named `is_prime` which takes an integer as an argument and returns true if the argument is a prime number, or false otherwise. Use the function in a program that prompts the user to enter a number then displays a message indicating whether the number is prime.
+
+*Divide by one 
+*Divide by itself
+
+Input --> Take an integer, pass it as an argument to a function --> returns true or false. 
+
--- prime-numbers/main.py
+++ prime-numbers/main.py
@@ -1,8 +1,38 @@
 #
-# Name
-# Date
+# Christian Espinoza Celis 
+# 01/25/2025
 # Rock, Paper, Scissors Game Programming Project
 # SDEV 1200
 #
 
 # Use comments liberally throughout the program. 
+
+# Input --> Take an integer, pass it as an argument to a function --> returns true or false. 
+
+def is_prime(given):
+    acc = 0
+    test = 0
+    torf = 0
+    test_numbers = [1,2,3,4,5,6,7,8]
+    if given == 2:
+        given = True
+        return given 
+    else: 
+        for i in test_numbers:
+            test = int(given/i)
+        if acc == 1:
+            given = True
+            return True
+        else: 
+            given = False 
+            return given
+
+
+checking = int(input("Pleae provide a number: "))
+
+checking = is_prime(checking)
+
+print(f'The number given is a prime number: {checking}')
+
+
+
