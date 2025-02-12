94e6f6d362bc1bd713e8570cb758fae003aa8bc5
Christian <christianespinozacel@westernwyoming.edu>
2025-02-12 20:38:21 +0000

    small name change

--- cash-register/main.py
+++ cash-register/main.py
@@ -1,8 +1,18 @@
 #
-# Name
-# Date
+# Christian Espinoza Celis 
+# 02/12/2024
 # Cash Register Programming Project
 # SDEV 1200
 #
 
 # Use comments liberally throughout the program. 
+
+#Gets the RetailItem class from the retailitem-class program 
+#It is important to note that the file had to be renamed due to the use of a hyphen conflicting.
+
+from retailitem_class.main import RetailItem
+
+print(RetailItem)
+        
+
+
